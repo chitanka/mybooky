212e70981ab0cd93bd622c7fdb18d1dad612163c
borislav <b.manolov@gmail.com>
2014-03-20 08:07:39 +0100

    move navbar to a separate template

--- application/views/home/index.blade.php
+++ application/views/home/index.blade.php
@@ -4,35 +4,17 @@
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width">
 	<title>MyBooky</title>
-	<link rel="stylesheet" type="text/css" href="{{asset('bundles/scaffold/css/bootstrap.css')}}">
+	<link rel="stylesheet" type="text/css" href="{{asset('css/bootstrap.min.css')}}">
+	<style>
+		body { margin: 1em; }
+	</style>
 </head>
 <body>
 	<div class="container">
-		<h1>MyBooky</h1>
-		<ul>
-			<li>{{ HTML::link('books', __('admin.title_books_index')) }}</li>
-			<li>{{ HTML::link('contents', __('admin.title_contents_index')) }}</li>
-		</ul>
-		Личности
-		<ul>
-			<li>{{ HTML::link('authors', __('admin.title_authors_index')) }}</li>
-			<li>{{ HTML::link('translators', __('admin.title_translators_index')) }}</li>
-			<li>{{ HTML::link('compilers', __('admin.title_compilers_index')) }}</li>
-			<li>{{ HTML::link('illustrators', __('admin.title_illustrators_index')) }}</li>
-		</ul>
-		Издатели
-		<ul>
-			<li>{{ HTML::link('publishers', __('admin.title_publishers_index')) }}</li>
-			<li>{{ HTML::link('printhouses', __('admin.title_printhouses_index')) }}</li>
-			<li>{{ HTML::link('sequences', __('admin.title_sequences_index')) }}</li>
-			<li>{{ HTML::link('themes', __('admin.title_themes_index')) }}</li>
-		</ul>
-		Допълнителни
-		<ul>
-			<li>{{ HTML::link('sequences', __('admin.title_sequences_index')) }}</li>
-			<li>{{ HTML::link('themes', __('admin.title_themes_index')) }}</li>
-			<li>{{ HTML::link('languages', __('admin.title_languages_index')) }}</li>
-		</ul>
+		<?php echo render('navbar') ?>
+		<p>Привет!</p>
 	</div>
+	<script src="{{asset('js/jquery-1.11.0.min.js')}}"></script>
+	<script src="{{asset('js/bootstrap.min.js')}}"></script>
 </body>
 </html>
--- application/views/layouts/admin.blade.php
+++ application/views/layouts/admin.blade.php
@@ -7,56 +7,12 @@
 	<link rel="stylesheet" type="text/css" href="{{asset('css/bootstrap.min.css')}}">
 	<link href="{{asset('select2-3.4.5/select2.css')}}" rel="stylesheet">
 	<style>
-		body { margin: 40px; }
+		body { margin: 1em; }
 	</style>
 </head>
 <body>
 	<div class="container">
-		<div class="navbar navbar-default" role="navigation">
-			<div class="container-fluid">
-				<div class="navbar-header">
-					<button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
-						<span class="icon-bar"></span>
-						<span class="icon-bar"></span>
-						<span class="icon-bar"></span>
-					</button>
-					<a class="navbar-brand" href="/">MyBooky</a>
-				</div>
-				<div class="navbar-collapse collapse">
-					<ul class="nav navbar-nav">
-						<li>{{ HTML::link('books', __('admin.title_books_index')) }}</li>
-						<li>{{ HTML::link('contents', __('admin.title_contents_index')) }}</li>
-						<li class="dropdown">
-							<a href="#" class="dropdown-toggle" data-toggle="dropdown">Личности <b class="caret"></b></a>
-							<ul class="dropdown-menu">
-								<li>{{ HTML::link('authors', __('admin.title_authors_index')) }}</li>
-								<li>{{ HTML::link('translators', __('admin.title_translators_index')) }}</li>
-								<li>{{ HTML::link('compilers', __('admin.title_compilers_index')) }}</li>
-								<li>{{ HTML::link('illustrators', __('admin.title_illustrators_index')) }}</li>
-							</ul>
-						</li>
-						<li class="dropdown">
-							<a href="#" class="dropdown-toggle" data-toggle="dropdown">Издатели <b class="caret"></b></a>
-							<ul class="dropdown-menu">
-								<li>{{ HTML::link('publishers', __('admin.title_publishers_index')) }}</li>
-								<li>{{ HTML::link('printhouses', __('admin.title_printhouses_index')) }}</li>
-								<li>{{ HTML::link('sequences', __('admin.title_sequences_index')) }}</li>
-								<li>{{ HTML::link('themes', __('admin.title_themes_index')) }}</li>
-							</ul>
-						</li>
-						<li class="dropdown">
-							<a href="#" class="dropdown-toggle" data-toggle="dropdown">Допълнителни <b class="caret"></b></a>
-							<ul class="dropdown-menu">
-								<li>{{ HTML::link('sequences', __('admin.title_sequences_index')) }}</li>
-								<li>{{ HTML::link('themes', __('admin.title_themes_index')) }}</li>
-								<li>{{ HTML::link('languages', __('admin.title_languages_index')) }}</li>
-							</ul>
-						</li>
-					</ul>
-				</div><!--/.nav-collapse -->
-			</div><!--/.container-fluid -->
-		</div>
-
+		<?php echo render('navbar') ?>
 		<div class="row">
 			<div class="span16">
 				<h1>{{$title}}</h1>
