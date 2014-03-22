9e5da92b6c0448970705b7f6772cf7a0e39591b3
borislav <b.manolov@gmail.com>
2014-03-22 10:52:22 +0100

    fix 'list' url in edit mode

--- application/views/crud/edit.blade.php
+++ application/views/crud/edit.blade.php
@@ -28,7 +28,7 @@
 	<div class="form-group">
 		<div class="col-sm-offset-2 col-sm-10">
 			<button type="submit" class="btn btn-primary"><span class="glyphicon glyphicon-floppy-disk"></span> {{ __('admin.action_save') }}</button>
-			<a class="pull-right btn btn-default" href="{{ URL::to(Request::referrer()) }}"><span class="glyphicon glyphicon-list"></span> {{ __('admin.action_list') }}</a>
+			<a class="pull-right btn btn-default" href="{{ URL::to($key) }}"><span class="glyphicon glyphicon-list"></span> {{ __('admin.action_list') }}</a>
 		</div>
 	</div>
 {{ Form::close() }}
